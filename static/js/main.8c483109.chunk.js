9d10e7087fe27a6adbb1227f4b81968f9848eead
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-05-25 17:30:39 +0200

    Updates

--- static/js/main.8c483109.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreact_sum=this.webpackJsonpreact_sum||[]).push([[0],{10:function(t,e,c){"use strict";c.r(e);c(1);var n=c(3),s=c.n(n),b=function(t){return t.a+t.b},j=(c(8),c(0)),r=function(){return Object(j.jsxs)(j.Fragment,{children:[Object(j.jsx)(b,{a:2,b:3}),Object(j.jsx)(b,{a:-5,b:5}),Object(j.jsx)(b,{a:10,b:0}),Object(j.jsx)(b,{a:0,b:5}),Object(j.jsx)(b,{a:0,b:0})]})};s.a.render(Object(j.jsx)(r,{}),document.getElementById("root"))},8:function(t,e,c){}},[[10,1,2]]]);
-//# sourceMappingURL=main.8c483109.chunk.js.map
